0d902ee351c51645fdf02dd698030d3e329506f0
wisniowek <131484500+wisniowek@users.noreply.github.com>
2025-03-22 13:11:21 +0100

    main.py

--- main.py
+++ main.py
@@ -112,5 +112,5 @@ def search(query: str):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
